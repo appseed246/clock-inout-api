980d27493a8e33afbf4f115f640231b1e0d67230
appseed246 <guitar.telecaster2017@gmail.com>
2019-05-16 21:05:45 +0900

    lambda上での実行を確認

--- sample.js
+++ sample.js
@@ -6,6 +6,7 @@ const sample = async () => {
   let browser;
   let result = [];
   try {
+    console.log("------------ start ------------");
     browser = await pupetteer.launch({
       // headless: false,
       args: chromium.args,
@@ -13,15 +14,19 @@ const sample = async () => {
       executablePath: await chromium.executablePath,
       headless: chromium.headless
     });
+    console.log("------------ newPage ------------");
     const page = await browser.newPage();
 
+    console.log("------------ google ------------");
     await page.goto("https://www.google.com");
     await page.type("input[name='q']", "よもぎまる");
 
+    console.log("------------ submit ------------");
     // 検索フォームのsubmit実行
     page.$eval("form[name='f']", elm => elm.submit());
 
     // ページ遷移待機
+    console.log("------------ waiting ------------");
     await page.waitForNavigation();
 
     // 検索結果のページのタイトルをすべて取得
@@ -30,12 +35,12 @@ const sample = async () => {
       const content = await elem.getProperty("innerHTML");
       result.push(await content.jsonValue());
     }
+    console.log("------------ obtain ------------");
   } finally {
     if (browser != null) {
       await browser.close();
     }
   }
-
   // console.log(result);
   return result;
 };
